c49be9aebfe99b382e8f98cd2dd9285744eaa20e
root <root@auto-sp.paulstepowski.org>
2013-03-04 15:20:21 +1000

    .

--- puppet/modules/apache_web_server/manifests/init.pp
+++ puppet/modules/apache_web_server/manifests/init.pp
@@ -14,6 +14,21 @@ class apache_web_server {
 			ensure  => running,
 			require => Package["httpd"],
 		} # End if.
+
+		# Configure Apache for Shibboleth.
+		if ($SERVICE_PROVIDER_SOFTWARE_TYPE == 'shibboleth') {
+
+                	file { "/etc/httpd/conf.d/shib.conf":
+                        	owner   => root,
+                        	group   => root,
+                        	mode    => 644,
+                        	content => template("apache_web_server/shib.conf.erb"),
+                        	require => Package["httpd"],
+				notify  => Service["httpd"],
+                	} # End file.
+
+		} # End if
+
 	} # End if.
 
 } # End class.
