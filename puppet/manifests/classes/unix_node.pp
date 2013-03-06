0c40ee148592949956aa9b83fb11fa6a5c577c47
root <root@auto-sp.paulstepowski.org>
2013-03-06 13:49:29 +1000

    .

--- puppet/manifests/classes/unix_node.pp
+++ puppet/manifests/classes/unix_node.pp
@@ -1,6 +1,9 @@
 # Configuration applied to all UNIX-like servers.
 class unix_node {
 
+	include 'unix_ntp_client'
+
+	# Linux specific configuration.
 	if ($kernel == 'Linux') {
 		include 'linux_node'
 	} # End if.
